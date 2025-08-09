f806f9790d116df84ba0304c17e624b31fdc3d17
ahmadsoud <user@DESKTOP-HHD9T3B>
2025-08-09 18:33:21 +0300

    removed extra comments

--- FoodMart/Services/ICategoryService.cs
+++ FoodMart/Services/ICategoryService.cs
@@ -3,7 +3,7 @@ namespace FoodMart.Services
 {
     public interface ICategoryService
     { 
-        // 
+        
         void AddCategory(Category category, IFormFile ImageFile);
         List<Category> GetCategories();
         void UpdateCategory(Category category, IFormFile ImageFile);
